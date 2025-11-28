9ffac6ea04c428b816218c4a24157d911c24b948
sourabh kumar <lordgk02@gmail.com>
2025-11-28 20:30:28 +0530

    added unhandled transaction

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -1,3 +1,3 @@
 module.exports = {
-    bookingRepository: require('./booking-repository')
+    BookingRepository: require('./booking-repository')
 }
--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -3,20 +3,35 @@ const db = require("../models");
 const { FlightServiceUrl } = require("../config/server-config");
 const { AppError } = require("../utils");
 const { StatusCodes } = require("http-status-codes");
+const {BookingRepository} = require ("../repositories")
 
+const bookingRepository= new BookingRepository()
 
 async function createBooking(data) {
-    return new Promise((res , rej)=>{
-        const result = db.sequelize.transaction(async function bookingImpl(t){
-            const flight = await axios.get(`${FlightServiceUrl}/${data.flightId}`)
-            const flightData = flight.data.data
-            console.log('flightData',flightData)
-            if(data.noOfSeats > flightData.totalSeats){
-                rej(new AppError('Required no of seats not available',StatusCodes.INTERNAL_SERVER_ERROR))
-            }
-            res(true)
-        })
-    })
+
+    const transaction = db.sequelize.transaction()
+    try {
+        const flight = await axios.get(`${FlightServiceUrl}/${data.flightId}`)
+        const flightData = flight.data.data
+        console.log('flightData',flightData)
+        if(data.noOfSeats > flightData.totalSeats){
+            throw new AppError('Required no of seats not available',StatusCodes.INTERNAL_SERVER_ERROR)
+        }
+
+        const totalCost = flightData.price * data.noOfSeats
+
+        const bookingPayload = {... data, totalCost:totalCost}
+        bookingRepository.create(bookingPayload)
+
+        await transaction.commit()
+        return true
+    } catch (error) {
+        await transaction.rollback()
+    }
+    
+
+
+
     
 }
 
